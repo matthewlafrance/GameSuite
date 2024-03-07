af31ce06b556c3476678b393f65ceebe02529853
matthewlafrance <128189755+matthewlafrance@users.noreply.github.com>
2024-03-06 17:38:56 -0800

    structure for IO

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,73 @@
 import java.util.regex.*;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         String games = "Tic-Tac-Toe, Chess, 2048";
         System.out.println("Welcome to Matthew's GameSuite");
-        System.out.println("Please choose your game, current options are " + games);
+
         Scanner input = new Scanner(System.in);
-        String game = input.nextLine();
-        game.toLowerCase();
-        System.out.println(game);
-        boolean played = false;
-        while (!played) {
-            if (game.equalsIgnoreCase("Chess")) {
-                    Chess.play();
-                    played = true;
-            } else if (game.equals("2048")) {
-                    tfe.play();
-                    played = true;
-            } else if (Pattern.matches("[tT]ic[- ]?[Tt]ac[- ]?[Tt]oe", game)) {
-                    TicTac.play();
-                    played = true;
-            } else {
-                System.out.println("invalid game choice, please make sure input matches options exactly");
-                game = input.nextLine();
+        String game = promptFor("Start a new game with `new`, or load an existing one with `load`", input);
+
+        while (!(game.toLowerCase().equals("load") || game.toLowerCase().equals("new"))) {
+            game = promptFor("Please input either \'new\' or \'load\'", input);
+        } 
+
+        if (game.toLowerCase().equals("load")) {
+            game = promptFor("Please input the name of your loaded game file", input);
+            boolean validFile = false;
+            while (!validFile) {
+                try {
+                    BufferedReader in = new BufferedReader(new FileReader(game));
+                    String gameName = in.readLine();
+                    String gameState = in.readLine();
+                    if (gameName.equals("chess")) {
+                        Chess.play();
+                    } else if (gameName.equals("2048")) {
+                        tfe.play();
+                    } else if (gameName.equals("ticTacToe")) {
+                        TicTac.play();
+                    } else {
+                        game = promptFor("Invalid game name, please try again", input);
+                        continue;
+                    }
+                    validFile = true;
+                } catch (FileNotFoundException e) {
+                    game = promptFor("Invalid file name, please try again", input);
+                } catch (IOException e) {
+                    System.out.println("Failed to read file");
+                    System.exit(1);
+                }
+            }
+        } else {
+            game = promptFor("Please choose your game, current options are " + games, input);
+            game.toLowerCase();
+            System.out.println(game);
+            boolean played = false;
+            while (!played) {
+                if (game.equalsIgnoreCase("Chess")) {
+                        Chess.play();
+                        played = true;
+                } else if (game.equals("2048")) {
+                        tfe.play();
+                        played = true;
+                } else if (Pattern.matches("[tT]ic[- ]?[Tt]ac[- ]?[Tt]oe", game)) {
+                        TicTac.play();
+                        played = true;
+                } else {
+                    game = promptFor("invalid game choice, please make sure input matches options exactly", input);
+                }
             }
         }
+    }
+
+    public static String promptFor(String prompt, Scanner input) {
+        System.out.println(prompt);
+        System.out.print("> ");
+        return input.nextLine();
     }
 }
